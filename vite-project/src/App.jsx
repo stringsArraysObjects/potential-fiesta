a9273927ef6f4187f7024f18ec4485cbd1cfcf4d
Chris <chtuff@gmail.com>
2024-06-28 19:27:34 -0400

    playing around with the input search bar css, will attempt something animated tomorrow

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import Nav from "./Nav";
 import Sidebar from "./Sidebar";
 import MyCarousel from "./MyCarousel";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 export default function App(){
@@ -8,7 +9,9 @@ export default function App(){
    
     return (
        <>
-         <header><h1>Fantastic</h1></header>
+         <header><h1>Fantastic</h1>
+         <input type="search"  id="site-search" placeholder="search" />
+         </header>
          <MyCarousel/>
          <nav className="containter-nav">
             <Nav/>
