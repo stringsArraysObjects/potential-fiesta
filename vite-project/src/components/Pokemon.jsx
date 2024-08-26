f83a121e7fda6ae9f65b7406c45c55f21056a7a1
Chris <chtuff@gmail.com>
2024-08-26 09:54:08 -0400

    had to remove the alt attribute for the element to work

--- vite-project/src/components/Pokemon.jsx
+++ vite-project/src/components/Pokemon.jsx
@@ -15,14 +15,14 @@ function Pokemon() {
                 }
             };
             fetchData()
-            
+            console.log(data)
         },[])
         return (
             <div>
                 
                     
                     <h3>{data.name}</h3>
-                    <img src={data.sprites.other['official-artwork'].front_default} alt="" />
+                    <img src={data.sprites.other['official-artwork'].front_default} />
                 
                
             </div>
@@ -32,5 +32,39 @@ function Pokemon() {
 export default Pokemon
 
 
+// const url = `https://pokeapi.co/api/v2/pokemon/${name}`
+// const url2 = 'https://pokeapi.co/api/v2/ability/battle-armor'
+// getAbility(url2)
+// getData(url)
+// })
+
+// async function getAbility(url2) {
+// try {
+//  const response = await fetch(url2);
+//  if (!response.ok) {
+//    throw new Error(`Response status: ${response.status}`);
+//  }
+
+//  const data = await response.json();
+
+//   document.querySelector('#flavorHeader').innerHTML = 'flavor:'
+//   document.querySelector('#abilityPara').innerHTML = data.flavor_text_entries[10].flavor_text
+
+//  console.log(data);
+// } catch (error) {
+//    console.error(error.message);
+// }
+
+// }
 
 
+// async function getData(url) {
+// try {
+//  const response = await fetch(url);
+//  if (!response.ok) {
+//    throw new Error(`Response status: ${response.status}`);
+//  }
+
+//  const data = await response.json();
+//  document.querySelector('#staticNamePic').src =  data.sprites.other['official-artwork'].front_default   
+//  document.querySelector('#staticName').innerText = data.name
